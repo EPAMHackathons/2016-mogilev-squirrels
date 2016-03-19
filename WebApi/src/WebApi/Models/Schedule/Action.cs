8e0f728bd5feab7429abada1ec767ad7cd89ff8f
s-kruglikov <stas.kruglikov@gmail.com>
2016-03-19 10:16:22 +0300

    Added entities for schedule controller

---
+++ WebApi/src/WebApi/Models/Schedule/Action.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Models
+{
+    public class Action
+    {
+	    public string Id { get; set; }
+
+	    public string Value { get; set; }
+
+	    public string Description { get; set; }
+    }
+}
---
+++ WebApi/src/WebApi/Models/Schedule/UserSchedule.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WebApi.Models
+{
+    public class UserSchedule
+    {
+	    public virtual ApplicationUser User { get; set; }
+
+	    public List<DaySchedule> Schedule { get; set; }
+    }
+}
