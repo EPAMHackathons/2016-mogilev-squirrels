823e5658a6cec61e10e553f024fdb879c7cc5a8c
Valentin <valicser@yandex.ru>
2016-03-19 11:32:54 +0300

    Fixed

--- Frontend/src/app/core/navbar/navbar.component.ts
+++ Frontend/src/app/core/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
 export class Navbar implements ng.IComponentOptions {
-    public controller: 'NavbarController';
-    public templateUrl: 'core/navbar/navbar.tpl.html';
+    public controller: string = 'NavbarController';
+    public templateUrl: string = 'core/navbar/navbar.tpl.html';
 }
--- Frontend/src/app/pages/main/main.module.ts
+++ Frontend/src/app/pages/main/main.module.ts
@@ -1,9 +1,11 @@
 import '../../core/core.module';
 
 import {configureStates} from './main.route';
+import {MainController} from './main.controller';
 
 angular
     .module('app.pages.main', [
         'app.core'
     ])
+    .controller('MainController', MainController)
     .config(configureStates);
