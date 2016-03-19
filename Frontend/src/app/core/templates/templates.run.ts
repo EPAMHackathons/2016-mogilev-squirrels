e29c933ee5c9a138df94f533c12363677463467b
Dzianis Pasiukou <Dzianis_Pasiukou1@epam.com>
2016-03-19 11:31:27 +0300

    add calendar

--- Frontend/src/app/core/templates/templates.run.ts
+++ Frontend/src/app/core/templates/templates.run.ts
@@ -1,4 +1,4 @@
-angular.module("app.core.templates").run(["$templateCache", function($templateCache) {$templateCache.put("calendar/calendar.tpl.html","");
+angular.module("app.core.templates").run(["$templateCache", function($templateCache) {$templateCache.put("calendar/calendar.tpl.html","<div id=\"graphContainer\" class=\"graphContainer\">\r\n</div>");
 $templateCache.put("core/404.tpl.html","<h1>404</h1>");
 $templateCache.put("gallery/gallery.tpl.html","");
 $templateCache.put("success/success.tpl.html","<h1>Наши успехи</h1>");
